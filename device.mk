fa0e48912bb4054dd56f9ff82880afbe44a979ca
Ksawlii <maja.blaszczak@proton.me>
2025-08-10 17:59:11 +0200

    a33x: Init
    Change-Id: I7de4c771dd17a5afc567e6745b51563ecaa7b4b6
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ device.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/samsung/a33x/a33x-vendor.mk)
+
+# Inherit the common makefile from common tree
+$(call inherit-product, device/samsung/s5e8825-common/common.mk)
+
+DEVICE_PATH := device/samsung/a33x
