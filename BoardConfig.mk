020681b0840954d460130dda8ec012be79f02b81
Ksawlii <maja.blaszczak@proton.me>
2025-08-10 18:03:18 +0200

    a33x: Initial properties
    Change-Id: Ib2d0374db9ceca8655dd0373fd1b21bc9dc01f7a
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,3 +20,6 @@ include vendor/samsung/a33x/BoardConfigVendor.mk
 include device/samsung/s5e8825-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/samsung/a33x
+
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
