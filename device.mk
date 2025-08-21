ef016ebc95ed9352bf3fa73e0e613f5341886f10
Majaahh <maja.blaszczak@proton.me>
2025-08-21 15:17:08 +0200

    a33x: Initial SM-A336E support
    Change-Id: Ic1f9b5a7caa68ebe303e8e92b6de696dfe36560f
    Co-authored-by: Salvo Giangreco <giangrecosalvo9@gmail.com>
    Co-authored-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- device.mk
+++ device.mk
@@ -43,6 +43,18 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Placeholders
+EMPTY_PLACEHOLDER := $(DEVICE_PATH)/configs/placeholder
+
+PRODUCT_COPY_FILES += \
+    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/AP_AUDIO_SLSI.bin \
+    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/APDV_AUDIO_SLSI.bin \
+    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/calliope_sram.bin \
+    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/mfc_fw.bin \
+    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/os.checked.bin \
+    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/NPU.bin \
+    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/vts.bin
+
 # UDFPS
 $(call soong_config_set,samsung_udfps,udfps_zorder,0x2000U)
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/samsung/fingerprint:libudfps_extension.samsung)
